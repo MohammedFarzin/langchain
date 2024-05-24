8fa6e20c414e8341446a24d248cecc8525565ef2
MohammedFarzin <mohammedfarzin.m123@gmail.com>
2024-05-24 14:58:57 +0530

    feat: Add langchain_objectbox to app.py and requirements.txt

--- objectbox/app.py
+++ objectbox/app.py
@@ -1,2 +1,15 @@
+import os
 import streamlit as st
 from langchain.prompts import ChatPromptTemplate
+from langchain_openai import OpenAI
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_groq import ChatGroq
+from langchain_objectbox import ObjectBox
+from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from dotenv import load_dotenv
+load_dotenv()
+
+
